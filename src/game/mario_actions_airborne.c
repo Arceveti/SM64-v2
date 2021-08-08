fbf53c859989a225927ec32cf4f37e6a3322aae5
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-08-08 10:56:00 -0700

    Improve air turn again

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -169,6 +169,7 @@ void update_air_with_turn(struct MarioState *m) {
 #ifdef AIR_TURN
     f32 sidewaysSpeed = 0.0f;
     s32 turnRange;
+    f32 absYVel = ABS(m->vel[1]);
 #endif
     f32 dragThreshold;
     s16 intendedDYaw;
@@ -181,8 +182,8 @@ void update_air_with_turn(struct MarioState *m) {
             intendedMag  = m->intendedMag / 32.0f;
             m->forwardVel += 1.5f * coss(intendedDYaw) * intendedMag;
 #ifdef AIR_TURN
-            if (m->forwardVel >= 4.0f || (m->forwardVel < 4.0f && ABS(m->vel[1]) > 32.0f)) {
-                turnRange = min((m->intendedMag - m->forwardVel) * ABS(m->vel[1]), 0x4000);
+            if (m->forwardVel >= 4.0f || (m->forwardVel < 4.0f && absYVel > 32.0f) || (absYVel > 64.0f)) {
+                turnRange = min(((m->intendedMag - m->forwardVel) * absYVel), 0x4000);
                 if (turnRange < 0x100) turnRange = 0x100;
                 m->faceAngle[1] = m->intendedYaw - approach_s32(intendedDYaw, 0x0, turnRange, turnRange);
             }
